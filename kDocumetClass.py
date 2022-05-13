1985ab469bd20a4971678212420223a54d68dbf9
hsdgxoafadsvc <hsdgxoafadsvc44mqtpsovpasfer@gmail.com>
2022-05-13 12:18:50 +0300

    крайняя версия

--- kDocumetClass.py
+++ kDocumetClass.py
@@ -2,6 +2,8 @@ import pythoncom
 from win32com.client import Dispatch, gencache
 import LDefin2D
 import MiscellaneousHelpers as MH
+import sys
+from pathlib import Path
 
 class kAPPLICATION:
 
@@ -23,21 +25,23 @@ class kAPPLICATION:
 
       def open(self, path):
             self.docList.append(kDOCUMENT(self.APP.Documents.Open(path), self.kAPI7, self.kAPI5, self.kObject))
+      def getActiveDocument(self):
+            self.docList.append(kDOCUMENT(self.APP.ActiveDocument, self.kAPI7, self.kAPI5, self.kObject))
 
 
 class kDOCUMENT:
 
       def __init__(self, iKompasDocument, ikAPI7, ikAPI5, ikObject):
-            self.kompas_document = iKompasDocument
+            self.kDocument = iKompasDocument
             self.kAPI7 = ikAPI7
             self.kAPI5 = ikAPI5
             self.kObject = ikObject
-            self.kompas_document_2d = self.kAPI7.IKompasDocument2D(self.kompas_document)
+            self.kDocument2D = self.kAPI7.IKompasDocument2D(self.kDocument)
             #  Получим активный документ
             # iDocument2D = self.kObject.ActiveDocument2D()
-            self.ViewsCount = self.kAPI7.IKompasDocument2D(self.kompas_document).ViewsAndLayersManager.Views.Count
-            self.iDrawingDocument = self.kAPI7.IDrawingDocument(self.kompas_document._oleobj_.QueryInterface(self.kAPI7.IDrawingDocument.CLSID,pythoncom.IID_IDispatch))
-            self.SheetsCount = self.kompas_document.LayoutSheets.Count
+            self.ViewsCount = self.kAPI7.IKompasDocument2D(self.kDocument).ViewsAndLayersManager.Views.Count
+            self.iDrawingDocument = self.kAPI7.IDrawingDocument(self.kDocument._oleobj_.QueryInterface(self.kAPI7.IDrawingDocument.CLSID, pythoncom.IID_IDispatch))
+            self.SheetsCount = self.kDocument.LayoutSheets.Count
             self.TextsInView = 0
             self.TablesInView = 0
             self.RoughsCount = 0
@@ -74,45 +78,60 @@ class kDOCUMENT:
             print(type(application))
             print(type(Documents))
             print("iDocument2D", type(iDocument2D))
-            print("kompas_document", type(self.kompas_document))
+            print("kDocument", type(self.kDocument))
             # iDocument2D.ksLineSeg(53.278238290749, 221.464629752129, 122.397130580339, 137.181898847987, 1)
             # iDocument2D.orthoMode
             # print(type(iDocument2D))
-            # print(type(kompas_document))
-            # print(kompas_document.LayoutSheets)
-            # print(kompas_document.LayoutSheets)
-            # kompas_document.LayoutSheets.Item(1).Format.Format вовращает формат листа
+            # print(type(kDocument))
+            # print(kDocument.LayoutSheets)
+            # print(kDocument.LayoutSheets)
+            # kDocument.LayoutSheets.Item(1).Format.Format вовращает формат листа
             # print(kompas_object.Coutn)
 
-            # kompas_document.LayoutSheets.Item(1).Format.VerticalOrientation = True
-            # kompas_document.LayoutSheets.Item(1).Update()
+            # kDocument.LayoutSheets.Item(1).Format.VerticalOrientation = True
+            # kDocument.LayoutSheets.Item(1).Update()
 
-            # application.kompas_document.LayoutSheets.Item(1)
+            # application.kDocument.LayoutSheets.Item(1)
             # print(dir(kompas6_api5_module))
 
             # doc2D = application.ActiveDocument._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingDocument'], pythoncom.IID_IDispatch)
 
             # doc2D = self.kAPI7.IDrawingDocument(doc2D)
-            # fuck = self.kAPI7.IKompasDocument2D(kompas_document).ViewsAndLayersManager.Views.View(1)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'], pythoncom.IID_IDispatch)
+            # fuck = self.kAPI7.IKompasDocument2D(kDocument).ViewsAndLayersManager.Views.View(1)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'], pythoncom.IID_IDispatch)
 
             # print(dir(application.ActiveDocument._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingDocument'], pythoncom.IID_IDispatch)))
             # print(dir(self.kAPI7))
-            # print(self.kAPI7.IKompasDocument2D(kompas_document).ViewsAndLayersManager.Views.View(1)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'], pythoncom.IID_IDispatch))
-            # print(dir(self.kAPI7.IDrawingContainer(self.kAPI7.IKompasDocument2D(kompas_document).ViewsAndLayersManager.Views.View(1)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'], pythoncom.IID_IDispatch)).DrawingTexts.Count ))
+            # print(self.kAPI7.IKompasDocument2D(kDocument).ViewsAndLayersManager.Views.View(1)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'], pythoncom.IID_IDispatch))
+            # print(dir(self.kAPI7.IDrawingContainer(self.kAPI7.IKompasDocument2D(kDocument).ViewsAndLayersManager.Views.View(1)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'], pythoncom.IID_IDispatch)).DrawingTexts.Count ))
       def contents(self):
             i = 0
             while (i < self.ViewsCount):
-                  iSymbols2DContainer = self.kAPI7.IKompasDocument2D(self.kompas_document).ViewsAndLayersManager.Views.View(i)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['ISymbols2DContainer'], pythoncom.IID_IDispatch)
+                  iSymbols2DContainer = self.kAPI7.IKompasDocument2D(self.kDocument).ViewsAndLayersManager.Views.View(i)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['ISymbols2DContainer'], pythoncom.IID_IDispatch)
                   iSymbols2DContainer = self.kAPI7.ISymbols2DContainer(iSymbols2DContainer)
-                  self.TextsInView += self.kAPI7.IDrawingContainer(self.kAPI7.IKompasDocument2D(self.kompas_document).ViewsAndLayersManager.Views.View(i)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'], pythoncom.IID_IDispatch)).DrawingTexts.Count
+                  self.TextsInView += self.kAPI7.IDrawingContainer(self.kAPI7.IKompasDocument2D(self.kDocument).ViewsAndLayersManager.Views.View(i)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'], pythoncom.IID_IDispatch)).DrawingTexts.Count
                   self.TablesInView += iSymbols2DContainer.DrawingTables.Count
                   self.RoughsCount += iSymbols2DContainer.Roughs.Count
                   self.LineDimensionsCount += iSymbols2DContainer.LineDimensions.Count
                   i += 1
-
+      def getStamp(self, n):
+            return self.kDocument.LayoutSheets.Item(0).Stamp.Text(n).Str
+            #self.kDocument.LayoutSheets.Item(0).Update()
+            #self.kDocument.LayoutSheets.Item(0).Stamp.Update()
+            #print(self.kDocument.LayoutSheets.Item(0).Stamp.Text(2).Str)
+            #print(dir(self.kDocument.LayoutSheets.Item(0).Stamp.Text(2).Str))
+      def getFormatList(self):
+            i = 0
+            ListInfo = ""
+            ListInfo += " " + str(self.SheetsCount) + " лист(ов):"
+            while i < self.SheetsCount:
+                  ListInfo += " A" + str(self.kDocument.LayoutSheets.Item(i).Format.Format)
+                  if self.kDocument.LayoutSheets.Item(i).Format.FormatMultiplicity > 1:
+                        ListInfo += "x" + str(self.kDocument.LayoutSheets.Item(i).Format.FormatMultiplicity)
+                  i += 1
+            return ListInfo
       def showDrawContent(self):
 
-            print("Документ", self.kompas_document.Name, "cодержит: \n",
+            print("Документ", self.kDocument.Name, "cодержит: \n",
                   self.SheetsCount, "лист(а)\n",
                   self.ViewsCount, "вид(a/ов)\n",
                   self.TextsInView, "текст(ов)\n",
@@ -130,7 +149,7 @@ class kDOCUMENT:
             # string in text
             i = 0
             while (i < self.ViewsCount):
-                  iDrawingContainer = self.kAPI7.IKompasDocument2D(self.kompas_document).ViewsAndLayersManager.Views.View(i)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'],pythoncom.IID_IDispatch)
+                  iDrawingContainer = self.kAPI7.IKompasDocument2D(self.kDocument).ViewsAndLayersManager.Views.View(i)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['IDrawingContainer'],pythoncom.IID_IDispatch)
                   iDrawingContainer = self.kAPI7.IDrawingContainer(iDrawingContainer)
                   iDrawingText = iDrawingContainer.DrawingTexts
                   ViewTextCount = iDrawingText.Count
@@ -173,13 +192,13 @@ class kDOCUMENT:
             l2 = 3.5
             _y = y + l1 + l2
             x1 = 27
-            self.iDrawingDocument.TechnicalDemand.BlocksGabarits = [411, 62 + 3.5, 587.5, 100]
+            # = [411, 62 + 3.5, 587.5, 100]
             #[x, y, x + LTechDam, HTechDam]
             A4V = [27, _y, 27 + LTechDam, _y + HTechDam]
             #A4G =
             A3V = [114, _y, 114 + LTechDam, _y + HTechDam]
             A3G = [237, _y, 237 + LTechDam, _y + HTechDam]
-
+            self.iDrawingDocument.TechnicalDemand.BlocksGabarits = A3G
             #print(self.iDrawingDocument.TechnicalDemand.BlocksGabarits)
             self.iDrawingDocument.TechnicalDemand.Update()
                   #BlocksGabarits
@@ -195,7 +214,7 @@ class kDOCUMENT:
       def tableReplace(self, __old, __new):
             i = 0
             while (i < self.ViewsCount):
-                  iSymbols2DContainer = self.kAPI7.IKompasDocument2D(self.kompas_document).ViewsAndLayersManager.Views.View(i)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['ISymbols2DContainer'], pythoncom.IID_IDispatch)
+                  iSymbols2DContainer = self.kAPI7.IKompasDocument2D(self.kDocument).ViewsAndLayersManager.Views.View(i)._oleobj_.QueryInterface(self.kAPI7.NamesToIIDMap['ISymbols2DContainer'], pythoncom.IID_IDispatch)
                   iSymbols2DContainer = self.kAPI7.ISymbols2DContainer(iSymbols2DContainer)
                   TableViewCount = iSymbols2DContainer.DrawingTables.Count
                   if TableViewCount == 0:
@@ -227,5 +246,58 @@ class kDOCUMENT:
 
 kAPPLICATION = kAPPLICATION()
 
-kAPPLICATION.open(r"C:\Users\borod\OneDrive\Рабочий стол\new2.cdw")
-kAPPLICATION.docList[0].showDrawContent()
+
+class DirectionTree(object):
+    """Создать дерево каталогов
+         @ путь: целевой каталог
+         @ filename: имя файла для сохранения
+    """
+    workPath = r"H:\YandexDisk\Травматология и ортопедия\Блокируемые LCP\Лит О1 (2)\Пластины прямые"
+    wFile = r"C:\Users\User\Desktop\Новый текстовый документ.txt"
+
+    def __init__(self, pathname=workPath, filename=wFile):
+        super(DirectionTree, self).__init__()
+        self.pathname = Path(pathname)
+        self.filename = filename
+        self.tree = ""
+
+    def set_path(self, pathname):
+        self.pathname = Path(pathname)
+
+    def set_filename(self, filename):
+        self.filename = filename
+
+    def generate_tree(self, n=0):
+        if self.pathname.is_file():
+            if "cdw" in self.pathname.name:
+                self.tree += '    ' * n + ' ' * 4 + self.pathname.name
+                #print(self.pathname)
+                kAPPLICATION.open(self.pathname)
+                self.tree += kAPPLICATION.docList[0].getFormatList()
+                self.tree += "\tРазрaб.: " + kAPPLICATION.docList[0].getStamp(110) + ','
+                self.tree += " Пров.: " + kAPPLICATION.docList[0].getStamp(111) + '\n'
+                kAPPLICATION.docList.pop()
+            else:
+                pass
+        elif self.pathname.is_dir():
+            self.tree += '    ' * n + ' ' * 4 + \
+                str(self.pathname.relative_to(self.pathname.parent)) + ':' + '\n'
+            for cp in self.pathname.iterdir():
+                self.pathname = Path(cp)
+                self.generate_tree(n + 1)
+
+    def save_file(self):
+        with open(self.filename, 'w', encoding='utf-8') as f:
+            f.write(self.tree)
+def max_str_len(tree):
+      max_str_len = 0
+      for str in tree:
+           if len(str) > max_str_len:
+                max_str_len += len(str)
+      return max_str_len
+
+
+dirtree = DirectionTree()
+dirtree.generate_tree()
+dirtree.save_file()
+print(dirtree.tree)
